2efa1a1a7281a936b20d8e54f95568949c0ab86e
codot-spec <exform.arc@gmail.com>
2024-09-14 06:01:59 -0700

    New attribute added in class.js

--- class.js
+++ class.js
@@ -1,20 +1,22 @@
 class MyClass {
-  constructor(name, age, city) {
+  constructor(name, age, city,email) {
     this.name = name;
     this.age = age;
     this.city = city;
+    this.email=email;
   }
 
   printDetails() {
     console.log(`Name: ${this.name}`);
     console.log(`Age: ${this.age}`);
     console.log(`City: ${this.city}`);
+    console.log(`email: ${this.email}`);
   }
 }
 
 // Create an instance of the class
-const myInstance = new MyClass("John Doe", 30, "New York");
-const myInstance2 = new MyClass("Jane Doe", 30, "California");
+const myInstance = new MyClass("John Doe", 30, "New York", "ab@g.com");
+const myInstance2 = new MyClass("Jane Doe", 30, "California", "de@g.com");
 // Print the constructor elements
 myInstance.printDetails();
 myInstance2.printDetails();
